6a462cd5e46792f1f88185bdb04008438b6f76c8
Rujun Chen <rujche@microsoft.com>
2024-06-06 09:39:17 +0800

    Create project by Spring Initlizer.

---
+++ src/main/java/rujche/problem/reproduce/ProblemReproduceApplication.java
@@ -0,0 +1,13 @@
+package rujche.problem.reproduce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemReproduceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProblemReproduceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rujche/problem/reproduce/ProblemReproduceApplicationTests.java
@@ -0,0 +1,13 @@
+package rujche.problem.reproduce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemReproduceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
